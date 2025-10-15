afffee87a0e8ba26af22def2639426f70aab3c19
Ziyad <zsyz8335@gmai.com>
2025-10-15 20:59:58 +0300

    Ziyad Alzahrnani

---
+++ main.py
@@ -0,0 +1,23 @@
+from panda import Panda
+
+p1 = Panda("Po", 5, "Black & White", 100)
+p2 = Panda("Ling", 3, "Brown", 85)
+p3 = Panda("Bao", 4, "Black & White", 95)
+p4 = Panda("Tao", 6, "Red", 70)
+
+print(p1.name)
+print(p2.name)
+print(p3.name)
+print(p4.name)
+
+p1.eat()
+p1.sleep()
+
+p2.eat()
+p2.sleep()
+
+p3.eat()
+p3.sleep()
+
+p4.eat()
+p4.sleep()
---
+++ panda.py
@@ -0,0 +1,13 @@
+
+class Panda:
+    def __init__(self, name, age, color, weight):
+        self.name = name
+        self.age = age
+        self.color = color
+        self.weight = weight
+
+    def eat(self):
+        print(f"{self.name} is eating bamboo.")
+
+    def sleep(self):
+        print(f"{self.name} is sleeping peacefully.")
